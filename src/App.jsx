0af7e02b01869821f7bc74732ed19a414c496ca5
jagadeesh <jagadeeshdegala10@gmail.com>
2026-01-19 14:57:03 +0530

    Revert "Fix build: remove vite.svg reference"
    This reverts commit 0047ffa21436252a9e617cd941ad4b2b9930505c.

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import { Routes, Route } from 'react-router-dom'
 //import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
 //import './App.css'
 import Header from './Header.jsx'
 import HeroSection from './HeroSection'
